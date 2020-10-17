04634f7229eeaa2712e36fc232ac1e39dafa93fd
Heesu Kim <63194988+lucakim27@users.noreply.github.com>
2020-10-17 15:05:29 +0900

    Python_sudoku_solver_completed
    Perfect

--- Automatic_Solving_Sudoku.py
+++ Automatic_Solving_Sudoku.py
@@ -1,82 +1,82 @@
 a_1 = " "
 a_2 = " "
-a_3 = 3
-a_4 = " "
-a_5 = 4
-a_6 = 2
-a_7 = " "
-a_8 = 9
-a_9 = " "
-b_1 = " "
-b_2 = 9
+a_3 = " "
+a_4 = 2
+a_5 = 6
+a_6 = " "
+a_7 = 7
+a_8 = " "
+a_9 = 1
+b_1 = 6
+b_2 = 8
 b_3 = " "
 b_4 = " "
-b_5 = 6
+b_5 = 7
 b_6 = " "
-b_7 = 5
-b_8 = " "
+b_7 = " "
+b_8 = 9
 b_9 = " "
-c_1 = 5
-c_2 = " "
+c_1 = 1
+c_2 = 9
 c_3 = " "
 c_4 = " "
 c_5 = " "
-c_6 = " "
-c_7 = " "
-c_8 = 1
+c_6 = 4
+c_7 = 5
+c_8 = " "
 c_9 = " "
-d_1 = " "
-d_2 = " "
-d_3 = 1
-d_4 = 7
+d_1 = 8
+d_2 = 2
+d_3 = " "
+d_4 = 1
 d_5 = " "
 d_6 = " "
-d_7 = 2
-d_8 = 8
-d_9 = 5
+d_7 = " "
+d_8 = 4
+d_9 = " "
 e_1 = " "
 e_2 = " "
-e_3 = 8
-e_4 = " "
+e_3 = 4
+e_4 = 6
 e_5 = " "
-e_6 = " "
-e_7 = 1
+e_6 = 2
+e_7 = 9
 e_8 = " "
 e_9 = " "
-f_1 = 3
-f_2 = 2
-f_3 = 9
+f_1 = " "
+f_2 = 5
+f_3 = " "
 f_4 = " "
 f_5 = " "
-f_6 = 8
-f_7 = 7
-f_8 = " "
-f_9 = " "
+f_6 = 3
+f_7 = " "
+f_8 = 2
+f_9 = 8
 g_1 = " "
-g_2 = 3
-g_3 = " "
-g_4 = " "
+g_2 = " "
+g_3 = 9
+g_4 = 3
 g_5 = " "
 g_6 = " "
 g_7 = " "
-g_8 = " "
-g_9 = 1
+g_8 = 7
+g_9 = 4
 h_1 = " "
-h_2 = " "
-h_3 = 5
+h_2 = 4
+h_3 = " "
 h_4 = " "
-h_5 = 9
+h_5 = 5
 h_6 = " "
 h_7 = " "
-h_8 = 2
-h_9 = " "
-i_1 = " "
-i_2 = 8
-i_3 = " "
-i_4 = 2
+h_8 = 3
+h_9 = 6
+i_1 = 7
+i_2 = " "
+i_3 = 3
+i_4 = " "
 i_5 = 1
-i_6 = " "
-i_7 = 6
+i_6 = 8
+i_7 = " "
 i_8 = " "
 i_9 = " "
 
@@ -100,51 +100,36 @@ box7 = [g_1, g_2, g_3, h_1, h_2, h_3, i_1, i_2, i_3]
 box8 = [g_4, g_5, g_6, h_4, h_5, h_6, i_4, i_5, i_6]
 box9 = [g_7, g_8, g_9, h_7, h_8, h_9, i_7, i_8, i_9]
 
-boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
-
 alpha = [a, b, c, d, e, f, g, h, i]
 
 
 def Showing_map():
-    global alpha, a,b,c,d,e,f,g,h,i, boxes, box1,box2,box3,box4,box5,box6,box7,box8,box9
-    global a_1,a_2,a_3,a_4,a_5,a_6,a_7,a_8,a_9
-    global b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8,b_9
-    global c_1,c_2,c_3,c_4,c_5,c_6,c_7,c_8,c_9
-    global d_1,d_2,d_3,d_4,d_5,d_6,d_7,d_8,d_9
-    global e_1,e_2,e_3,e_4,e_5,e_6,e_7,e_8,e_9
-    global f_1,f_2,f_3,f_4,f_5,f_6,f_7,f_8,f_9
-    global g_1,g_2,g_3,g_4,g_5,g_6,g_7,g_8,g_9
-    global h_1,h_2,h_3,h_4,h_5,h_6,h_7,h_8,h_9
-    global i_1,i_2,i_3,i_4,i_5,i_6,i_7,i_8,i_9
-
-
-    print(" " + str(a_1) + "  |  " + str(a_2) + "  |  " + str(a_3) + "  |  " + str(a_4) + "  |  " + str(a_5) +
-          "  |  " + str(a_6) + "  |  " + str(a_7) + "  |  " + str(a_8) + "  |  " + str(a_9))
+    print(" " + str(a[0]) + "  |  " + str(a[1]) + "  |  " + str(a[2]) + "  |  " + str(a[3]) + "  |  " + str(a[4]) +
+          "  |  " + str(a[5]) + "  |  " + str(a[6]) + "  |  " + str(a[7]) + "  |  " + str(a[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(b_1) + "  |  " + str(b_2) + "  |  " + str(b_3) + "  |  " + str(b_4) + "  |  " + str(b_5) +
-          "  |  " + str(b_6) + "  |  " + str(b_7) + "  |  " + str(b_8) + "  |  " + str(b_9))
+    print(" " + str(b[0]) + "  |  " + str(b[1]) + "  |  " + str(b[2]) + "  |  " + str(b[3]) + "  |  " + str(b[4]) +
+          "  |  " + str(b[5]) + "  |  " + str(b[6]) + "  |  " + str(b[7]) + "  |  " + str(b[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(c_1) + "  |  " + str(c_2) + "  |  " + str(c_3) + "  |  " + str(c_4) + "  |  " + str(c_5) +
-          "  |  " + str(c_6) + "  |  " + str(c_7) + "  |  " + str(c_8) + "  |  " + str(c_9))
+    print(" " + str(c[0]) + "  |  " + str(c[1]) + "  |  " + str(c[2]) + "  |  " + str(c[3]) + "  |  " + str(c[4]) +
+          "  |  " + str(c[5]) + "  |  " + str(c[6]) + "  |  " + str(c[7]) + "  |  " + str(c[8]))
     print("----------------|-----------------|----------------")
-    print(" " + str(d_1) + "  |  " + str(d_2) + "  |  " + str(d_3) + "  |  " + str(d_4) + "  |  " + str(d_5) +
-          "  |  " + str(d_6) + "  |  " + str(d_7) + "  |  " + str(d_8) + "  |  " + str(d_9))
+    print(" " + str(d[0]) + "  |  " + str(d[1]) + "  |  " + str(d[2]) + "  |  " + str(d[3]) + "  |  " + str(d[4]) +
+          "  |  " + str(d[5]) + "  |  " + str(d[6]) + "  |  " + str(d[7]) + "  |  " + str(d[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(e_1) + "  |  " + str(e_2) + "  |  " + str(e_3) + "  |  " + str(e_4) + "  |  " + str(e_5) +
-          "  |  " + str(e_6) + "  |  " + str(e_7) + "  |  " + str(e_8) + "  |  " + str(e_9))
+    print(" " + str(e[0]) + "  |  " + str(e[1]) + "  |  " + str(e[2]) + "  |  " + str(e[3]) + "  |  " + str(e[4]) +
+          "  |  " + str(e[5]) + "  |  " + str(e[6]) + "  |  " + str(e[7]) + "  |  " + str(e[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(f_1) + "  |  " + str(f_2) + "  |  " + str(f_3) + "  |  " + str(f_4) + "  |  " + str(f_5) +
-          "  |  " + str(f_6) + "  |  " + str(f_7) + "  |  " + str(f_8) + "  |  " + str(f_9))
+    print(" " + str(f[0]) + "  |  " + str(f[1]) + "  |  " + str(f[2]) + "  |  " + str(f[3]) + "  |  " + str(f[4]) +
+          "  |  " + str(f[5]) + "  |  " + str(f[6]) + "  |  " + str(f[7]) + "  |  " + str(f[8]))
     print("----------------|-----------------|----------------")
-    print(" " + str(g_1) + "  |  " + str(g_2) + "  |  " + str(g_3) + "  |  " + str(g_4) + "  |  " + str(g_5) +
-          "  |  " + str(g_6) + "  |  " + str(g_7) + "  |  " + str(g_8) + "  |  " + str(g_9))
+    print(" " + str(g[0]) + "  |  " + str(g[1]) + "  |  " + str(g[2]) + "  |  " + str(g[3]) + "  |  " + str(g[4]) +
+          "  |  " + str(g[5]) + "  |  " + str(g[6]) + "  |  " + str(g[7]) + "  |  " + str(g[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(h_1) + "  |  " + str(h_2) + "  |  " + str(h_3) + "  |  " + str(h_4) + "  |  " + str(h_5) +
-          "  |  " + str(h_6) + "  |  " + str(h_7) + "  |  " + str(h_8) + "  |  " + str(h_9))
+    print(" " + str(h[0]) + "  |  " + str(h[1]) + "  |  " + str(h[2]) + "  |  " + str(h[3]) + "  |  " + str(h[4]) +
+          "  |  " + str(h[5]) + "  |  " + str(h[6]) + "  |  " + str(h[7]) + "  |  " + str(h[8]))
     print("- - - - - - - - | - - - - - - - - | - - - - - - - -")
-    print(" " + str(i_1) + "  |  " + str(i_2) + "  |  " + str(i_3) + "  |  " + str(i_4) + "  |  " + str(i_5) +
-          "  |  " + str(i_6) + "  |  " + str(i_7) + "  |  " + str(i_8) + "  |  " + str(i_9))
-
+    print(" " + str(i[0]) + "  |  " + str(i[1]) + "  |  " + str(i[2]) + "  |  " + str(i[3]) + "  |  " + str(i[4]) +
+          "  |  " + str(i[5]) + "  |  " + str(i[6]) + "  |  " + str(i[7]) + "  |  " + str(i[8]))
 
 def Solver():
     """
@@ -153,16 +138,18 @@ def Solver():
     3, if there's only one left num then put in
     4, otherwise, pass it
     """
-    global alpha, a,b,c,d,e,f,g,h,i, boxes, box1,box2,box3,box4,box5,box6,box7,box8,box9
-    global a_1,a_2,a_3,a_4,a_5,a_6,a_7,a_8,a_9
-    global b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8,b_9
-    global c_1,c_2,c_3,c_4,c_5,c_6,c_7,c_8,c_9
-    global d_1,d_2,d_3,d_4,d_5,d_6,d_7,d_8,d_9
-    global e_1,e_2,e_3,e_4,e_5,e_6,e_7,e_8,e_9
-    global f_1,f_2,f_3,f_4,f_5,f_6,f_7,f_8,f_9
-    global g_1,g_2,g_3,g_4,g_5,g_6,g_7,g_8,g_9
-    global h_1,h_2,h_3,h_4,h_5,h_6,h_7,h_8,h_9
-    global i_1,i_2,i_3,i_4,i_5,i_6,i_7,i_8,i_9
+    
+    # global alpha, a,b,c,d,e,f,g,h,i, box1,box2,box3,box4,box5,box6,box7,box8,box9
+    # global a_1,a_2,a_3,a_4,a_5,a_6,a_7,a_8,a_9
+    # global b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8,b_9
+    # global c_1,c_2,c_3,c_4,c_5,c_6,c_7,c_8,c_9
+    # global d_1,d_2,d_3,d_4,d_5,d_6,d_7,d_8,d_9
+    # global e_1,e_2,e_3,e_4,e_5,e_6,e_7,e_8,e_9
+    # global f_1,f_2,f_3,f_4,f_5,f_6,f_7,f_8,f_9
+    # global g_1,g_2,g_3,g_4,g_5,g_6,g_7,g_8,g_9
+    # global h_1,h_2,h_3,h_4,h_5,h_6,h_7,h_8,h_9
+    # global i_1,i_2,i_3,i_4,i_5,i_6,i_7,i_8,i_9
+ 
 
     for i in range(len(alpha)):
         "to get a,b,c,d,e,f,g,h,i"
@@ -233,9 +220,7 @@ def Solver():
                             numbers.append(box9[z])
 
 
-
             if len(numbers) == 8:
-                "checking the nums"
                 for q in range(1,10):
                     if not q in numbers:
                         if alpha[i][j] == " ":
@@ -246,20 +231,11 @@ def Solver():
 
 
 
-# def checker():
-#     """
-#     checking whether it's done or not.
-#     """
-#     for i in range(len(alpha)):
-#         for j in range(len(alpha[i])):
-#             print(alpha[i])
-
-
-
 Showing_map()
 print("--------------------------------------------------------------")
-
 Solver()
 Solver()
-
+Solver()
+Solver()
+Solver()
 Showing_map()
